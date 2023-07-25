cbcdf361a05269dc9c6c34a70ed8b517d62963d4
apollner <46967911+apollner@users.noreply.github.com>
2023-07-25 10:39:26 +0300

    Update bitcoin_price.py

--- bitcoin_price.py
+++ bitcoin_price.py
@@ -1,27 +1,40 @@
+import time
 import streamlit as st
-import requests
-import json
+import pandas as pd
+import cryptocompare
+from datetime import datetime
+import plotly.graph_objects as go
 
-# Get the current price of Bitcoin
-def get_bitcoin_price():
-  response = requests.get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
-  data = json.loads(response.content)
-  return data["amount"]
+# Create a dataframe to hold the Bitcoin price data
+price_data = pd.DataFrame(columns=['time', 'price'])
 
-# Create a real-time graph of the Bitcoin price
-def create_graph():
-  st.line_chart(get_bitcoin_price(), xaxis_label="Time", yaxis_label="Price (USD)")
+# Create a plotly figure with an initial trace
+fig = go.Figure(data=[go.Scatter(x=price_data['time'],
+                y=price_data['price'], mode='lines')])
 
-# Display the price of Bitcoin in number of bitcoin
-def display_price_in_bitcoin():
-  price = get_bitcoin_price()
-  st.write("The price of Bitcoin is currently $" + str(price))
-  st.write("This is equivalent to " + str(price / 1) + " bitcoin.")
+# Set up the Streamlit layout
+st.title('Real-time Bitcoin Price')
+price_text = st.empty()
+graph_placeholder = st.empty()
 
-# Main function
-def main():
-  create_graph()
-  display_price_in_bitcoin()
+# Update the Bitcoin price every second
+while True:
+    # Get the current price
+    price = cryptocompare.get_price('BTC', currency='USD')['BTC']['USD']
 
-if __name__ == "__main__":
-  main()
+    # Update the price text
+    price_text.text(f'Current Bitcoin price: ${price}')
+
+    # Append the current price and time to the dataframe
+    price_data = price_data.append(
+        {'time': datetime.now(), 'price': price}, ignore_index=True)
+
+    # Update the data of the plotly figure's first trace
+    fig.data[0].x = price_data['time']
+    fig.data[0].y = price_data['price']
+
+    # Display the plot in Streamlit
+    graph_placeholder.plotly_chart(fig)
+
+    # Wait for 1 second
+    time.sleep(1)
